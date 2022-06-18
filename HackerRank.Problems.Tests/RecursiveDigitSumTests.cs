e64ad2a2586e15897036a8004e373880eef17e7d
oleh-zheleznyak <oleh.zheleznyak@gmail.com>
2022-06-18 16:20:46 +0200

    Recursive Digit Sum

---
+++ HackerRank.Problems.Tests/RecursiveDigitSumTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HackerRank.Problems.Tests
+{
+    public class RecursiveDigitSumTests
+    {
+        [Theory]
+        [InlineData("1", 1, 1)]
+        [InlineData("11", 1, 2)]
+        [InlineData("1", 2, 2)]
+        [InlineData(" 9875", 4, 8)]
+        public void SuperDigitTest(string numberAsDirtyString, int timesToRepeat, int expectedSuperDigit)
+        {
+            var sut = new RecursiveDigitSum();
+            var actual = sut.SuperDigit(numberAsDirtyString, timesToRepeat);
+            Assert.Equal(expectedSuperDigit, actual);
+        }
+    }
+}
