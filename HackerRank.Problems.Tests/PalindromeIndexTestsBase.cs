d6183e2b10125a29e0a987f17df043550ba334f1
oleh.zheleznyak <oleh.zheleznyak@appfire.com>
2022-06-23 18:56:20 +0200

    implementation for palindrome index with tests

---
+++ HackerRank.Problems.Tests/PalindromeIndexTestsBase.cs
@@ -0,0 +1,26 @@
+﻿using HackerRank.Problems.Test2;
+using Xunit;
+
+namespace HackerRank.Problems.Tests
+{
+    public abstract class PalindromeIndexTestsBase
+    {
+        [Theory]
+        [InlineData("a", new[] { -1 })]
+        [InlineData("aa", new[] { -1 })]
+        [InlineData("ab", new[] { 0, 1 })]
+        [InlineData("abc", new[] { -1 })]
+        [InlineData("aba", new[] { -1 })]
+        [InlineData("abb", new[] { 0 })]
+        [InlineData("abca", new[] { 1, 2 })]
+        [InlineData("abcca", new[] { 1 })]
+        public void FindIndexToRemoveTests(string input, int[] allowedIndexes)
+        {
+            var sut = CreateSystemUnderTest();
+            var actualIndex = sut.FindIndexToRemove(input);
+            Assert.Contains(actualIndex, allowedIndexes);
+        }
+
+        protected abstract IPalindromeIndex CreateSystemUnderTest();
+    }
+}
--- HackerRank.Problems/PalindromeIndex.cs
+++ HackerRank.Problems/PalindromeIndex.cs
@@ -21,29 +21,42 @@ namespace HackerRank.Problems.Test2
             if (string.IsNullOrWhiteSpace(s)) return -1;
             if (s.Length == 1) return -1;
 
-            var editCount = 0;
-            var diffIndex = -1;
+            return FindIndexToRemoveGivenValidString(s);
+        }
+
+        private static int FindIndexToRemoveGivenValidString(string s)
+        {
+            // example -  a b c a
+            // move from both ends
+            //   if mismatch -> look one ahead (if possible) in both directions
+            //   if at least one match proceed
+            //   if more than one diff - return false
+            //   if not possible to look ahead return true
+
             var start = 0;
             var end = s.Length - 1;
+            var differenceFound = false;
+            var candidateIndex = -1;
+
             while (start < end)
             {
                 if (s[start] != s[end])
                 {
-                    editCount++;
+                    if (differenceFound) return -1;
+                    differenceFound = true;
 
-                    if (s[start + 1] == s[end])
+                    if (start + 1 == end) return start; // abca -> aca
+                    
+                    if (s[start + 1] == s[end]) // abcca -> acca
                     {
-                        diffIndex = start;
+                        candidateIndex = start;
                         start++;
                     }
-                    else
+                    else if (s[start] == s[end-1]) // accba -> acca
                     {
-                        diffIndex = end;
-                        end++;
+                        candidateIndex = end;
+                        end--;
                     }
-
-
-                    if (editCount > 1) return -1;
                 }
                 else
                 {
@@ -51,8 +64,7 @@ namespace HackerRank.Problems.Test2
                     end--;
                 }
             }
-
-            return diffIndex;
+            return candidateIndex;
         }
     }
 }
