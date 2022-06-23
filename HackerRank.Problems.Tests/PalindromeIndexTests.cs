961cf547c8de3f71871b1dcfb4af86027a2f1e85
oleh-zheleznyak <oleh.zheleznyak@gmail.com>
2022-06-23 18:56:20 +0200

    implementation for palindrome index with tests

---
+++ HackerRank.Problems.Tests/PalindromeIndexTests.cs
@@ -0,0 +1,15 @@
+﻿using HackerRank.Problems.Test2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HackerRank.Problems.Tests
+{
+    public class PalindromeIndexTests : PalindromeIndexTestsBase
+    {
+        protected override IPalindromeIndex CreateSystemUnderTest() => new PalindromeIndex();
+    }
+}
