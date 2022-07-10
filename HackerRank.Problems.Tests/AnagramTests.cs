41ecd7f676d9339a9da5be8aec2ff853182e7c8f
oleh-zheleznyak <oleh.zheleznyak@gmail.com>
2022-07-10 17:14:20 +0200

    bomberman - missed some states

---
+++ HackerRank.Problems.Tests/AnagramTests.cs
@@ -0,0 +1,25 @@
+﻿using HackerRank.Problems.Test4;
+using Xunit;
+
+namespace HackerRank.Problems.Tests
+{
+    public class AnagramTests
+    {
+        [Theory]
+        [InlineData("aaabbb",3)]
+        [InlineData("ab", 1)]
+        [InlineData("abc", -1)]
+        [InlineData("mnop", 2)]
+        [InlineData("xyyx", 0)]
+        [InlineData("xaxbbbxx", 1)]
+        [InlineData("asdfjoieufoa", 3)]
+        [InlineData("fdhlvosfpafhalll", 5)]
+        [InlineData("mvdalvkiopaufl", 5)]
+        public void EditDistanceTests(string input, int expectedEditDistance)
+        {
+            var sut = new Anagram();
+            var actualDistance = sut.EditDistance(input);
+            Assert.Equal(expectedEditDistance, actualDistance);
+        }
+    }
+}
---
+++ HackerRank.Problems.Tests/BetweenTwoSetsTests.cs
@@ -0,0 +1,18 @@
+﻿using HackerRank.Problems.Test3;
+using System.Linq;
+using Xunit;
+
+namespace HackerRank.Problems.Tests
+{
+    public class BetweenTwoSetsTests
+    {
+        [Theory]
+        [InlineData(new int[] { 2, 4 }, new int[] { 16, 32, 96 }, 3)]
+        public void GetTotalXTests(int[] a, int[] b, int expectedCount)
+        {
+            var sut = new BetweenTwoSets();
+            var actualCount = sut.CountTotalNumbersBetweenTwoSets(a.ToList(), b.ToList());
+            Assert.Equal(expectedCount, actualCount);
+        }
+    }
+}
