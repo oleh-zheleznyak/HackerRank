57d79a2c8a6f9353b0fb8f0623619d8e7af3ef61
oleh-zheleznyak <oleh.zheleznyak@gmail.com>
2022-07-18 20:57:55 +0200

    New year chaos using min heap

---
+++ HackerRank.Problems/NewYearChaos.cs
@@ -0,0 +1,29 @@
+﻿namespace HackerRank.Problems;
+
+public class NewYearChaos
+{
+    public int? CalculateNumberOfBribes(List<int> shuffeledQueue)
+    {
+        var minHeap = new MinHeap<int>(shuffeledQueue.Count+1);
+        minHeap.Push(shuffeledQueue[shuffeledQueue.Count - 1]);
+
+        var sum = 0;
+        var stack = new Stack<int>(3);
+        for (int i = shuffeledQueue.Count-2; i >=0; i--)
+        {
+            var counter = 0;
+            var value = shuffeledQueue[i];
+            while (minHeap.Count>0 && value > minHeap.Peek())
+            {
+                var removed = minHeap.Pop();
+                stack.Push(removed);
+                counter++;
+                if (counter > 2) return null;
+            }
+            sum += counter;
+            minHeap.Push(value);
+            while (stack.Count > 0) minHeap.Push(stack.Pop());
+        }
+        return sum;
+    }
+}
