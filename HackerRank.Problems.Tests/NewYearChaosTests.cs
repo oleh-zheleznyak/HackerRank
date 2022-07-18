aee79b5a27d535255184f125e5e6051d8ad50588
oleh.zheleznyak <oleh.zheleznyak@appfire.com>
2022-07-18 20:57:55 +0200

    New year chaos using min heap

---
+++ HackerRank.Problems.Tests/NewYearChaosTests.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace HackerRank.Problems.Tests
+{
+    public class NewYearChaosTests
+    {
+        [Theory]
+        [MemberData(nameof(GetTestData))]
+        public void CalculateNumberOfBribesTest(List<int> queue, int? expectedBribes)
+        {
+            var sut = new NewYearChaos();
+            var actual = sut.CalculateNumberOfBribes(queue);
+            Assert.Equal(expectedBribes, actual);
+        }
+
+        public static IEnumerable<object[]> GetTestData()
+        {
+            yield return SampleTestCase0();
+            yield return SampleTestCase0_TooChaotic();
+            yield return SampleTestCase1();
+            yield return SampleTestCase1_TooChaotic();
+            yield return SampleTestCase2();
+        }
+
+        private static object[] SampleTestCase0()
+        {
+            return new object[] {
+                new List<int>() { 2, 1, 5, 3, 4 },
+                3
+            };
+        }
+
+        private static object[] SampleTestCase0_TooChaotic()
+        {
+            return new object[] {
+                new List<int>() { 2, 5, 1, 3, 4 },
+                null
+            };
+        }
+
+        private static object[] SampleTestCase1()
+        {
+            return new object[] {
+                new List<int>() { 1, 2, 5, 3, 7, 8, 6, 4 },
+                //                0  0  2  0  2  2  1  0
+                7
+            };
+        }
+
+        private static object[] SampleTestCase1_TooChaotic()
+        {
+            return new object[] {
+                new List<int>() { 5, 1, 2, 3, 7, 8, 6, 4 },
+                null
+            };
+        }
+
+        private static object[] SampleTestCase2()
+        {
+            return new object[] {
+                new List<int>() { 1, 2, 5, 3, 4, 7, 8, 6 },
+                4
+            };
+        }
+    }
+}
---
+++ HackerRank.Problems/MinHeap.cs
@@ -0,0 +1,83 @@
+﻿namespace HackerRank.Problems
+{
+    public class MinHeap<T> where T: IComparable<T>
+    {
+        private T[] _storage;
+        private int _count;
+        private IComparer<T> comparer = Comparer<T>.Default; // todo: ability to supply via ctor
+
+        public MinHeap(IReadOnlyCollection<T> data)
+        {
+            if (data is null) throw new ArgumentNullException(nameof(data));
+            _count = data.Count;
+            _storage = data.ToArray();
+            BuildMinHeap();
+        }
+
+        public MinHeap(int capacity)
+        {
+            if (capacity < 0) throw new ArgumentOutOfRangeException(nameof(capacity));
+
+            _storage = new T[capacity];
+            _count = 0;
+        }
+
+        public int Count => _count; 
+
+        private void BuildMinHeap()
+        {
+            var n = _count / 2;
+            for (var i = n; i >= 0; i--)
+                RestoreHeapProperty(i);
+        }
+
+        public T Peek() => _count > 0 ? _storage[0] : throw new InvalidOperationException("heap is empty");
+
+        public T Pop()
+        {
+            if (_count <= 0) throw new InvalidOperationException("heap is empty");
+            var value = _storage[0];
+            _storage[0] = _storage[_count - 1];
+            _count--;
+            RestoreHeapProperty(0);
+            return value;
+        }
+        
+        public void Push(T value)
+        {
+            if (_count >= _storage.Length-1) throw new NotImplementedException("resizing not implemented");
+            _storage[_count] = value;
+            Swap(0, _count);
+            _count++;
+            RestoreHeapProperty(0);
+        }
+
+        private void RestoreHeapProperty(int index)
+        {
+            var value = _storage[index];
+
+            var leftIndex = LeftChild(index);
+            var rightIndex = RightChild(index);
+            int? nextIndex = null;
+
+            if (leftIndex < _count && comparer.Compare(_storage[leftIndex], value) < 0) nextIndex = leftIndex;
+            else if (rightIndex < _count && comparer.Compare(_storage[rightIndex], value) < 0) nextIndex = rightIndex;
+
+            if (nextIndex.HasValue)
+            {
+                Swap(nextIndex.Value, index);
+                RestoreHeapProperty(nextIndex.Value);
+            }
+        }
+
+        private int Parent(int index) => index >> 1;
+        private int LeftChild(int index) => index >> 1;
+        private int RightChild(int index) => index >> 1+1;
+        private void Swap(int i, int j)
+        {
+            var temp = _storage[i];
+            _storage[i] = _storage[j];
+            _storage[j] = temp;
+        }
+    }
+}
