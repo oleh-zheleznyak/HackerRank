cc6306afbff198b3396eef6d850a9f301d4098cb
oleh-zheleznyak <oleh.zheleznyak@gmail.com>
2022-08-28 17:07:58 +0200

    fixed overflows by adding mod, improved time complexity by adding cache

--- HackerRank.Problems.Tests/LegoBlocksTests.cs
+++ HackerRank.Problems.Tests/LegoBlocksTests.cs
@@ -16,6 +16,20 @@ public class LegoBlocksTests
     [InlineData(5, 4, 29791)]
     [InlineData(6, 4, 250047)]
     [InlineData(7, 4, 2048383)]
+    [InlineData(8,6,402844528)]
+    [InlineData(7,3,16129)]
+    [InlineData(3,8,328650)]
+    [InlineData(3,7,57252)]
+    [InlineData(8,10,634597424)]
+    [InlineData(3,4,343)]
+    [InlineData(10,3,1046529)]
+    [InlineData(10,8,797528970)]
+    [InlineData(2,10,3658)]
+    [InlineData(9,5,908059021)]
+    [InlineData(7,2,127)]
+    [InlineData(5,8,225730660)]
+    [InlineData(7,10,665164408)]
+    [InlineData(5,6,16164270)]
     public void NumberOfCombinationsForWallTest(int height, int width, int expectedCombinations)
     {
         var sut = new LegoBlocks();
--- HackerRank.Problems/LegoBlocks.cs
+++ HackerRank.Problems/LegoBlocks.cs
@@ -3,40 +3,71 @@ namespace HackerRank.Problems;
 public class LegoBlocks
 {
     private static readonly int[] BlockWidths = {1, 2, 3, 4};
+    private static readonly int Modulo = (int) Math.Pow(10, 9) + 7;
+    private readonly Dictionary<(int, int), int> powerModuloCache = new();
 
     public int NumberOfCombinationsForWall(int height, int width)
     {
+        if (height <= 0) throw new ArgumentException($"{nameof(height)} must be positive");
+        if (width <= 0) throw new ArgumentException($"{nameof(width)} must be positive");
+        if (width == 1) return 1;
+
+        var cache = new int?[width + 1];
         var rowDecompositions = CalculateRowDecompositions(width);
-        var result = NumberOfCombinationsForWall(height, width, rowDecompositions);
+        var result = NumberOfCombinationsForWall(height, width, rowDecompositions, cache);
         return result;
     }
 
     private int[] CalculateRowDecompositions(int width)
     {
         var rowDecompCache = new int?[width + 1];
-        var rowDecompositions = Enumerable.Range(0, width+1)
+        var rowDecompositions = Enumerable.Range(0, width + 1)
             .Select(x => NumberOfDecompositions(x, rowDecompCache))
             .ToArray();
         return rowDecompositions;
     }
 
-    private int NumberOfCombinationsForWall(int height, int width, int[] rowDecompositionsCache)
+    private int NumberOfCombinationsForWall(int height, int width, int[] rowDecompositionsCache, int?[] cache)
     {
-        var rowDecompositions = rowDecompositionsCache[width];
-        var allDecomp = (int) Math.Pow(rowDecompositions, height);
-
-        var allInvalidDecomp = 0;
-        for (var i = 1; i < width; i++)
+        checked
         {
-            var solidWallPrefix = NumberOfCombinationsForWall(height, i, rowDecompositionsCache);
-            // then there is a split in the wall at position between i and i+1
-            var allWallDecompositions = rowDecompositionsCache[width - i];
-            var anyWallSuffix = (int) Math.Pow(allWallDecompositions, height);
-            allInvalidDecomp += solidWallPrefix * anyWallSuffix;
+            if (cache[width].HasValue) return cache[width].Value;
+
+            var rowDecompositions = rowDecompositionsCache[width];
+            var allDecomp = PowModulo(rowDecompositions, height);
+
+            double allInvalidDecomp = 0;
+            for (var i = 1; i < width; i++)
+            {
+                var solidWallPrefix = NumberOfCombinationsForWall(height, i, rowDecompositionsCache, cache);
+                // then there is a split in the wall at position between i and i+1
+                var allWallDecompositions = rowDecompositionsCache[width - i];
+                var anyWallSuffix = PowModulo(allWallDecompositions, height);
+                allInvalidDecomp = (allInvalidDecomp + ((long)solidWallPrefix * anyWallSuffix) % Modulo) % Modulo;
+            }
+
+            var result = (int) (allDecomp - allInvalidDecomp + Modulo) % Modulo;
+            cache[width] = result;
+            return result;
+        }
+    }
+
+    private int PowModulo(int baseNum, int power)
+    {
+        if (powerModuloCache.TryGetValue((baseNum, power), out var cachedValue))
+            return cachedValue;
+        
+        var result = 1L;
+        for (var i = 1; i <= power; i++)
+        {
+            result = result * baseNum % Modulo;
         }
 
-        return allDecomp - allInvalidDecomp;
+        var computedValue = (int) (result + Modulo) % Modulo;
+        powerModuloCache[(baseNum, power)] = computedValue;
+        return computedValue;
     }
+        
 
     public int NumberOfDecompositions(int width, int?[] rowDecompCache)
     {
